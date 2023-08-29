8cc9009e107f933cfedfdc91ba8be992cf9e855d
Erkan Ozcanoglu <ozcanogerkan12@gmail.com>
2023-08-29 15:51:38 +0300

    gitignore

--- HRPortal.API/Program.cs
+++ HRPortal.API/Program.cs
@@ -11,6 +11,7 @@ namespace HRPortal.API {
 
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
+
             builder.Services.AddSwaggerGen();
             //builder.Services.AddDbContext<HRPortalContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("HRPortalContext")));
             // for postgresql
